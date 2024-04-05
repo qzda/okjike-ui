74142ffad4a33da2e2a7dd30407302f14e0b02ab
qzda <qinzhidao@foxmail.com>
2024-04-05 21:56:44 +0800

    feat: 基础结构

---
+++ components/Content.vue
@@ -0,0 +1,7 @@
+<script setup lang="ts">
+  const props = defineProps();
+</script>
+
+<template>
+  <h2>Content Vue component</h2>
+</template>
---
+++ unocss.config.ts
@@ -0,0 +1,35 @@
+import {
+  defineConfig,
+  presetAttributify,
+  presetIcons,
+  presetUno,
+  presetWebFonts,
+} from "unocss";
+
+export default defineConfig({
+  shortcuts: [
+    [
+      "link",
+      "cursor-pointer select-none transition duration-200 ease-in-out op-75 hover:op-100",
+    ],
+    ["xy-center", "flex justify-center items-center"],
+  ],
+  presets: [
+    presetUno(),
+    presetAttributify(),
+    presetIcons({
+      extraProperties: {
+        display: "inline-block",
+        height: "1.2em",
+        width: "1.2em",
+        "vertical-align": "text-bottom",
+      },
+    }),
+    presetWebFonts({
+      provider: "google",
+      fonts: {
+        mono: ["JetBrains Mono"],
+      },
+    }),
+  ],
+});
