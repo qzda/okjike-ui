f7dcf319b7323d41548d941a0635616f80c62e4a
qzda <qinzhidaoa@gmail.com>
2024-04-05 21:56:44 +0800

    feat: 基础结构

--- entrypoints/popup/App.vue
+++ entrypoints/popup/App.vue
@@ -1,30 +1,69 @@
 <script lang="ts" setup>
-import HelloWorld from '@/components/HelloWorld.vue';
+  import { onMounted, reactive, ref } from "vue";
+  import { browser } from "wxt/browser";
+  import { storage } from "wxt/storage";
+
+  onMounted(() => {
+    browser.runtime.sendMessage("message!");
+  });
+
+  const configRef = reactive<{
+    layout?: "waterfall" | "normal";
+  }>({ layout: "waterfall" });
 </script>
 
 <template>
-  <div>
-    <a href="https://wxt.dev" target="_blank">
-      <img src="/wxt.svg" class="logo" alt="WXT logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="@/assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+  <div class="popup-container">
+    <form class="form-config-container">
+      <div
+        v-for="layout in ['waterfall', 'normal']"
+        class="form-item"
+      >
+        <input
+          type="radio"
+          name="layout"
+          :value="layout"
+          :id="layout"
+          v-model="configRef.layout"
+        />
+        <label :for="layout">{{ layout }}</label>
+      </div>
+    </form>
+
+    <!-- <pre>{{ JSON.stringify(configRef, null, 2) }}</pre> -->
   </div>
-  <HelloWorld msg="WXT + Vue" />
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #54bc4ae0);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
+<style scoped lang="scss">
+  .popup-container {
+    width: 200px;
+    font-family: system-ui, -apple-system, BlinkMacSystemFont, "Segoe UI",
+      Roboto, Oxygen, Ubuntu, Cantarell, "Open Sans", "Helvetica Neue",
+      sans-serif;
+
+    > .form-config-container {
+      display: flex;
+      flex-direction: column;
+      gap: 0.5rem;
+
+      > .form-item {
+        label {
+          cursor: pointer;
+        }
+
+        display: flex;
+        justify-content: flex-start;
+        align-items: center;
+        gap: 0.2rem;
+
+        input {
+          margin: 0;
+        }
+      }
+    }
+
+    p {
+      font-size: 1rem;
+    }
+  }
 </style>
--- wxt.config.ts
+++ wxt.config.ts
@@ -1,5 +1,5 @@
-import { defineConfig } from 'wxt';
-import vue from '@vitejs/plugin-vue';
+import { defineConfig } from "wxt";
+import vue from "@vitejs/plugin-vue";
 
 // See https://wxt.dev/api/config.html
 export default defineConfig({
@@ -8,6 +8,14 @@ export default defineConfig({
       vueTemplate: true,
     },
   },
+  manifest: {
+    permissions: ["storage"],
+  },
+  runner: {
+    startUrls: ["https://web.okjike.com"],
+    chromiumArgs: ["--user-data-dir"],
+  },
+  outDir: "dist",
   vite: () => ({
     plugins: [vue()],
   }),
