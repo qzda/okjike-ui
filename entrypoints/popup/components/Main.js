04458c1391139c6ae23d4fef01600881f3406cd9
qzda <qinzhidao@foxmail.com>
2024-04-26 10:20:46 +0800

    feat(popup): 布局、配置项

--- entrypoints/popup/components/Main.js
+++ entrypoints/popup/components/Main.js
@@ -1,13 +1,11 @@
 import ExtensionStatus from "./sections/ExtensionStatus";
 import InterfaceSection from "./sections/InterfaceSection";
-// import NavigationSection from "./sections/NavigationSection";
 import TimelineSection from "./sections/TimelineSection";
 
 const Main = () => (
   <main className="flex flex-col p-2 gap-y-4">
     <ExtensionStatus />
     <TimelineSection />
-    {/* <NavigationSection /> */}
     <InterfaceSection />
   </main>
 );
--- entrypoints/popup/components/sections/InterfaceSection.js
+++ entrypoints/popup/components/sections/InterfaceSection.js
@@ -1,19 +1,16 @@
-import { KeyInterFont, KeySearchBar, KeyTitleNotifications, KeyTransparentSearch, KeyTweetButton } from "../../../../storage-keys";
+import { KeySearchBar, KeyTweetButton } from "../../../../storage-keys";
 import SectionLabel from "../ui/SectionLabel";
 import SwitchControl from "../ui/SwitchControl";
 
 const InterfaceSection = () => (
   <section className="flex flex-col gap-y-2">
-    <SectionLabel htmlFor="user-control-interface">Interface</SectionLabel>
+    <SectionLabel htmlFor="user-control-interface">界面</SectionLabel>
     <div id="user-control-interface">
       <form className="flex flex-col items-center justify-between px-4 dark:bg-x-bgTwoDark bg-x-bgTwo rounded-2xl">
         <div className="w-full py-4">
           <div className="flex flex-col gap-y-4">
-            <SwitchControl label="Inter Font" storageKey={KeyInterFont} />
-            <SwitchControl label="Search Bar" storageKey={KeySearchBar} />
-            <SwitchControl label="Transparent Search Bar" storageKey={KeyTransparentSearch} />
-            <SwitchControl label="Tweet Button" storageKey={KeyTweetButton} />
-            <SwitchControl label="Notifications in Title" storageKey={KeyTitleNotifications} />
+            <SwitchControl label="搜索栏" storageKey={KeySearchBar} />
+            <SwitchControl label="发帖" storageKey={KeyTweetButton} />
           </div>
         </div>
       </form>
