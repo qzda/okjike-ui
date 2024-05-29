ec666a9e01e7a8d8bbfd4f8c18065f87b4189f5e
qzda <qinzhidaoa@gmail.com>
2024-05-29 14:54:20 +0800

    feat: 支持隐藏侧边栏

--- entrypoints/content-scripts2/utils/all.ts
+++ entrypoints/content-scripts2/utils/all.ts
@@ -1,8 +1,15 @@
-import { KeyTimelineLayout, KeyTimelineWidth } from "../../../storage-keys"
+import {
+  KeySidebarHidden,
+  KeyTimelineLayout,
+  KeyTimelineWidth,
+} from "../../../storage-keys"
+import { changeKeySidebarHidden } from "./sidebar"
 import { changeTimelineLayout, changeTimelineWidth } from "./timeline"
 
 export function injectAllChanges(data: Record<string, string | number>) {
   changeTimelineWidth(+data[KeyTimelineWidth])
 
   changeTimelineLayout(data[KeyTimelineLayout].toString())
+
+  changeKeySidebarHidden(data[KeySidebarHidden].toString())
 }
