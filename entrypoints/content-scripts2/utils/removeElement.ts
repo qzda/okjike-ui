20a17b96204b18dfbcc8ea527244cd4cd5f77540
qzda <qinzhidao@foxmail.com>
2024-05-30 13:42:35 +0800

    feat: getYDistance

--- entrypoints/content-scripts2/utils/removeElement.ts
+++
@@ -1,7 +0,0 @@
-export function removeElementById(id: string) {
-  document.getElementById(id)?.remove()
-}
-
-export function removeElement(selector: string) {
-  document.querySelector(selector)?.remove()
-}
