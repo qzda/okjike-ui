08fa75a56f3bea662602968fe22713e4ca7e403a
qzda <qinzhidaoa@gmail.com>
2024-06-01 20:27:57 +0800

    refactor: changeTimelineLayout

--- entrypoints/content-scripts/utils/timeline.ts
+++ entrypoints/content-scripts/utils/timeline.ts
@@ -36,76 +36,76 @@ export function changeTimelineCardStyle() {
   )
 }
 
-const posts = new Set<HTMLDivElement>()
-const cardMinWidth = 400
-const gap = 8
-let pathname = ""
-
-function updatePosts() {
-  if (pathname !== location.pathname) {
-    pathname = location.pathname
-    posts.clear()
-  }
-  const _posts = [
-    ...(document.querySelector<HTMLDivElement>(
-      `${selectors.mainColumnItems.posts}`
-    )?.childNodes || []),
-  ].slice(0, -1)
-  _posts.forEach((_post) => {
-    posts.add(_post as HTMLDivElement)
-  })
-  console.log("posts.size", posts.size)
-}
-
-function initWaterfall() {
-  updatePosts()
-  getStorage(KeyTimelineWidth).then((val) => {
-    const timelineWidth = +val
-
-    const cols = (timelineWidth / cardMinWidth) >> 0
-    const postsGroupByCol = new Array(cols)
-      .fill(0)
-      .map<HTMLDivElement[]>(() => [])
-    let [postsGroupByColIndex, postsIndex] = [0, 0]
-    const _posts = [...posts.values()]
-    while (postsGroupByColIndex < cols && postsIndex < _posts.length) {
-      postsGroupByCol[postsGroupByColIndex].push(_posts[postsIndex])
-
-      postsIndex++
-      postsGroupByColIndex++
-      if (postsGroupByColIndex === cols) {
-        postsGroupByColIndex = 0
-      }
-    }
-
-    postsGroupByCol.forEach((posts) => {
-      for (let i = 1; i < posts.length; i++) {
-        const element = posts[i]
-        const previousElement = posts[i - 1]
-
-        const heightDiff =
-          ((
-            previousElement.firstChild as HTMLDivElement
-          )?.getBoundingClientRect().height || 0) -
-          previousElement.getBoundingClientRect().height
-
-        element.style.marginTop = `${heightDiff}px`
-      }
-    })
-  })
-}
-
-function cancelWaterfall() {
-  posts.forEach((post) => {
-    post.style.marginTop = ""
-  })
-  posts.clear()
-}
-
 /**
  * @param layout "default" or "waterfall"
  */
 export function changeTimelineLayout(layout: string) {
+  const posts = new Set<HTMLDivElement>()
+  const cardMinWidth = 400
+  const gap = 8
+  let pathname = ""
+
+  function updatePosts() {
+    if (pathname !== location.pathname) {
+      pathname = location.pathname
+      posts.clear()
+    }
+    const _posts = [
+      ...(document.querySelector<HTMLDivElement>(
+        `${selectors.mainColumnItems.posts}`
+      )?.childNodes || []),
+    ].slice(0, -1)
+    _posts.forEach((_post) => {
+      posts.add(_post as HTMLDivElement)
+    })
+    console.log("posts.size", posts.size)
+  }
+
+  function initWaterfall() {
+    updatePosts()
+    getStorage(KeyTimelineWidth).then((val) => {
+      const timelineWidth = +val
+
+      const cols = (timelineWidth / cardMinWidth) >> 0
+      const postsGroupByCol = new Array(cols)
+        .fill(0)
+        .map<HTMLDivElement[]>(() => [])
+      let [postsGroupByColIndex, postsIndex] = [0, 0]
+      const _posts = [...posts.values()]
+      while (postsGroupByColIndex < cols && postsIndex < _posts.length) {
+        postsGroupByCol[postsGroupByColIndex].push(_posts[postsIndex])
+
+        postsIndex++
+        postsGroupByColIndex++
+        if (postsGroupByColIndex === cols) {
+          postsGroupByColIndex = 0
+        }
+      }
+
+      postsGroupByCol.forEach((posts) => {
+        for (let i = 1; i < posts.length; i++) {
+          const element = posts[i]
+          const previousElement = posts[i - 1]
+
+          const heightDiff =
+            ((
+              previousElement.firstChild as HTMLDivElement
+            )?.getBoundingClientRect().height || 0) -
+            previousElement.getBoundingClientRect().height
+
+          element.style.marginTop = `${heightDiff}px`
+        }
+      })
+    })
+  }
+
+  function cancelWaterfall() {
+    posts.forEach((post) => {
+      post.style.marginTop = ""
+    })
+    posts.clear()
+  }
+
   if (layout === "waterfall") {
     addStyles(
       "timelineLayout",
