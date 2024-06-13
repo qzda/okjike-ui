ebff076298c56fb8d9a600aa61f8fc825482383b
qzda <qinzhidaoa@gmail.com>
2024-06-13 16:47:57 +0800

    feat: 浏览器控制台支持颜色log

--- utils/log.ts
+++ utils/log.ts
@@ -2,5 +2,17 @@
  * @example log(1, 2, 3) // "okjike-ui", 1, 2, 3
  */
 export function log(...arg: any[]) {
-  console.log("okjike-ui", ...arg)
+  console.log(logBgBlack(logColorYellowLight("okjike-ui")), ...arg)
+}
+
+export function logColorCyan(str: string) {
+  return `\x1b[36m${str}\x1b[0m`
+}
+
+export function logColorYellowLight(str: string) {
+  return `\x1b[93m${str}\x1b[0m`
+}
+
+export function logBgBlack(str: string) {
+  return `\x1b[40m${str}\x1b[0m`
 }
