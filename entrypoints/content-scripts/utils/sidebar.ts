8faa60c300382753fbfcce5f407985e3f83ce529
qzda <qinzhidao@foxmail.com>
2024-06-26 15:16:41 +0800

    feat(KeyHiddenSidebar): sidebar显示时增加动画

--- entrypoints/content-scripts/utils/sidebar.ts
+++ entrypoints/content-scripts/utils/sidebar.ts
@@ -1,8 +1,42 @@
 import selectors from "../selectors"
-import { addStyles, removeStyles } from "./style"
+import { addStyles } from "./style"
+import { isTimelineUrl } from "./timeline"
 
-export function changeKeySidebar(hidden: string) {
-  if (hidden === "on") {
+export function changeKeyHiddenSidebar(hidden: string, pathname: string) {
+  if (isTimelineUrl(pathname) && hidden === "off") {
+    addStyles(
+      "hiddenSidebar",
+      `
+      @keyframes showSideBar {
+        0% {
+          right: -100%;
+        }
+        100% {
+          right: 0%;
+        }
+      }
+      ${selectors.sideBar} {
+        position: fixed;
+        right: -100%;
+        height: fit-content;
+        padding: 8px;
+        margin: 0;
+        gap: 8px;
+        animation: showSideBar 0.2s ease-in-out forwards;
+      }
+      ${selectors.sideBar} > div {
+        margin: 0;
+        border-radius: 10px;
+        overflow: hidden;
+        box-shadow:
+          4px 8px 8px rgba(128, 128, 128, 0.5);
+      }
+      ${selectors.sideBar} > footer {
+        display: none;
+      }
+      `
+    )
+  } else {
     addStyles(
       "hiddenSidebar",
       `
@@ -11,7 +45,5 @@ export function changeKeySidebar(hidden: string) {
       }
       `
     )
-  } else {
-    removeStyles("hiddenSidebar")
   }
 }
