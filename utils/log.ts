6576f269a641d1f2e8f5068cf6a2104829481824
qzda <qinzhidaoa@gmail.com>
2024-08-27 22:24:48 +0800

    feat(utils): 添加devLog

--- utils/log.ts
+++ utils/log.ts
@@ -1,6 +1,15 @@
-import prolog from "@qzda/prolog"
-import { name, version } from "../package.json"
+import prolog from "@qzda/prolog";
+import { name, version } from "../package.json";
 
 export function log(...arg: any[]) {
-  console.log(prolog.bgBlack(prolog.brightYellow(`${name} ${version}`)), ...arg)
+  console.log(
+    prolog.bgBlack(prolog.brightYellow(`${name} ${version}`)),
+    ...arg
+  );
+}
+
+export function devLog(...arg: any[]) {
+  if (process.env.NODE_ENV === "dev") {
+    log(...arg);
+  }
 }
