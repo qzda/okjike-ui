47435a5a1bf4528d52ff7e7a9e939e549e72fde6
qzda <qinzhidaoa@gmail.com>
2024-08-29 15:37:32 +0800

    feat: 放弃浏览器插件方案

--- entrypoints/popup/vite.config.ts
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from "vite"
-import react from "@vitejs/plugin-react"
-import unocss from "unocss/vite"
-
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react(), unocss()],
-})
